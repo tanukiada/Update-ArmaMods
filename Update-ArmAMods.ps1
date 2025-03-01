78720e0c7b5c0d32532b9185fe3aaedbe53792cb
Jaiden Taylor <jtaylord17@gmail.com>
2025-03-01 17:16:53 -0600

    First commit

---
+++ Update-ArmAMods.ps1
@@ -0,0 +1,68 @@
+<#
+    .SYNOPSIS
+    To be written..
+#>
+
+function main {
+    $modList = Get-Mods
+    foreach ($mod in $modList) {
+        $modID = Get-ModID($mod)
+        $updateTimestampRemote = Get-UpdateTimestampRemote($modID)
+        $updateTimestampLocal = Get-UpdateTimestampLocal($modID)
+        $dateTimeLocal = [datetime]::FromBinary($updateTimestampLocal)
+        $unixTimeLocal = [DateTimeOffset]::new($dateTimeLocal).ToUnixTimeSeconds()
+        $status = Compare-Times($unixTimeLocal, $updateTimestampRemote, $mod)
+        if ($status) {
+            Write-Output "$mods needs updated."
+        } elseif (!$status) {
+            Write-Output "$mod is up to date."
+        }
+    }
+    Set-Location "C:\Users\james\Documents\Coding\powershell\Update-ArmAMods"
+}
+
+function Get-Mods {
+    $mods = @(Get-ChildItem -Filter '@*' C:/Arma3/ | Select-Object -ExpandProperty Name)
+    return $mods
+}
+
+function Get-MetaFile($mod) {
+    Set-Location -Path "C:\Arma3\$mod"
+    $metaFileHash = Get-Content 'meta.cpp' -Raw | ConvertFrom-StringData -Delimiter '='
+    return $metaFileHash
+}
+
+function Get-ModID($mod) {
+    Set-Location -Path "C:\Arma3\$mod"
+    $metaFileHash = Get-Content 'meta.cpp' -Raw | ConvertFrom-StringData -Delimiter '='
+    return $metaFileHash['publishedid'].TrimEnd(";")
+    
+}
+
+function Get-UpdateTimestampRemote($id) {
+    $body = @{
+        itemcount = 1
+        'publishedfileids[0]' = $id
+    }
+    $request = Invoke-WebRequest -Method 'POST' -Uri "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/" -Headers @{charset="utf-8"} -Body $body | Select-Object -Expand "Content" | ConvertFrom-Json
+    $request = $request.response | Select-Object -Expand "publishedfiledetails"
+    $updateTimestamp = $request | Select-Object -Expand "time_updated"
+    return $updateTimestamp
+}
+
+function Get-UpdateTimestampLocal($id) {
+    Set-Location -Path "C:\Arma3\$mod"
+    $metaFileHash = Get-Content 'meta.cpp' -Raw | ConvertFrom-StringData -Delimiter '='
+    return $metaFileHash['timestamp'].TrimEnd(";")
+}
+
+function Compare-Times($localTime, $remoteTime, $mod) {
+
+    if ($remoteTime -gt $localTime) {
+        return $true
+    } elseif ($remoteTime -le $localTime) {
+        return $false
+    }
+}
+
+main
